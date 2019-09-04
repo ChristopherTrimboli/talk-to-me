0f1663af622e699bb3df0253a1e439701d740e0e
Chris <chris.eddy.dev@gmail.com>
2019-09-04 01:26:30 -0700

    cleanup code, sqlQuery promise, login refactor

--- backend/index.js
+++ backend/index.js
@@ -15,77 +15,91 @@ app.use(express.json());
 http.createServer(app).listen(http_port, () => { console.log(`Talk to Me listening on port: ${http_port}!`) })
 https.createServer(app).listen(https_port, () => { console.log(`Talk to Me listening on port: ${https_port}!`) })
 
-const isEmailDuplicate = async (email) => {
+const isEmailDuplicate = email => {
     return new Promise((resolve, reject) => {
         try{
-            pool.query('SELECT email from users WHERE email = ?', [email],
-            function (error, results, fields) {
+            pool.query('SELECT email from users WHERE email = ?', [email], (error, results) => {
                 if (error) {
-                    resolve(true);
+                    reject(error)
                 }
                 else if(results.length > 0){ // email exists
-                    resolve(true);
+                    resolve(true)
                 }
                 else if(results.length === 0){ // email does not exist
-                    resolve(false);
+                    resolve(false)
                 }
             });
         }
         catch(e){
-            reject(e);
+            reject(e)
         }
     });
 }
 
-app.post('/register', async function (req, res) {
+const sqlQuery = (query, params) => {
+    return new Promise((resolve, reject) => {
+        try{
+            pool.query(query, params, (error, results) => {
+                if (error) {
+                    console.log(error)
+                    reject(error)
+                }
+                else{
+                    resolve(results)
+                }
+            });
+        }
+        catch(e){
+            reject(e)
+        }
+    });
+}
+
+app.post('/register', async (req, res) => {
     await isEmailDuplicate(req.body.email)
-    .then((isDuplicate) => {
+    .then(isDuplicate => {
         if(isDuplicate){
-            res.status(500).send({message: 'Email already exists'});
+            res.status(500).send({message: 'Email already exists'})
         }
         else{
             const saltRounds = 10;
-            bcrypt.hash(req.body.password, saltRounds, function(err, hashedPass) {
-                pool.query('INSERT INTO users (email, password, first_name, last_name) VALUES (?, ?, ?, ?)',
-                [req.body.email, hashedPass, req.body.firstName, req.body.lastName],
-                function (error, results, fields) {
-                    if (error) {
-                        res.status(500).send(error);
-                    }
-                    else {
-                        res.status(200).send({message: 'Registered succesfully'});
-                    }
-                });
+            bcrypt.hash(req.body.password, saltRounds, async (err, hashedPass) => {
+                await sqlQuery(
+                    'INSERT INTO users (email, password, first_name, last_name) VALUES (?, ?, ?, ?)',
+                    [req.body.email, hashedPass, req.body.firstName, req.body.lastName]
+                ).then(results => {
+                    res.status(200).send({message: 'Registered succesfully'})
+                }).catch(error => {
+                    res.status(500).send(error)
+                })
             });
         }
     })
-    .catch((error) => {
+    .catch(error => {
         console.log(error)
-        res.status(500).send({error: 'Caught error in: /register'});
+        res.status(500).send({error: 'Caught error in: isEmailDuplicate()'})
     })
 })
 
-app.post('/login', function (req, res) {
-    pool.query('SELECT * FROM users WHERE email = ?', [req.body.email],
-    function (error, results, fields) {
-        if (error) {
-            res.status(500).send(error);
-            console.log(error)
-        }
-        else if(results.length > 0){ // if email exists check password hash
-            console.log(results[0])
+app.post('/login', async (req, res) => {
+    await sqlQuery('SELECT * FROM users WHERE email = ?', [req.body.email])
+    .then(results => {
+        if(results.length > 0){
             bcrypt.compare(req.body.password, results[0].password, function(err, isMatch) {
-                if(isMatch === true){
-                    res.sendStatus(200);
+                if(isMatch){
+                    delete results[0].password
+                    res.status(200).send({message: 'Login Successful', user: results[0]})
                 }
                 else{
-                    res.sendStatus(500);
+                    res.status(500).send({message: 'Login failed, incorrect credentials'})
                 }
             });
         }
-        else{ // email does not exist
-            res.sendStatus(500);
+        else{
+            res.status(500).send({message: 'Login failed, email does not exist'})
         }
-    });
+    }).catch(error => {
+        res.status(500).send(error)
+    })
 })
 
