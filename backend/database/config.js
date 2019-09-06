5fc15ad72a7b0785811fa99eae6c42b8dee4dc57
Chris <chris.eddy.dev@gmail.com>
2019-09-06 02:03:46 -0700

    add database node env travis

--- backend/database/config.js
+++ backend/database/config.js
@@ -1,15 +1,32 @@
 const mysql = require('mysql');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'HelloThere69',
-  database: 'talk-to-me',
-  insecureAuth : true,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+let pool = {}
+
+if(process.env.NODE_ENV === "production"){
+  pool = mysql.createPool({
+    host: 'talk-to-me.cilhwpqjm37r.us-west-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'admin',
+    password: process.env.DATABASE_PASS,
+    database: 'talk-to-me',
+    insecureAuth : true,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+}
+else{
+  pool = mysql.createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'HelloThere69',
+    database: 'talk-to-me',
+    insecureAuth : true,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+}
 
 module.exports = pool;
